68cd70a124b4ba5049ba3880a2c86d162e80272d
RafaelAvilac <rafael_avilac@yahoo.com.br>
2025-07-19 18:10:49 -0300

    Create IF_ElseIf_Else.js

---
+++ javaScript/IF_ElseIf_Else.js
@@ -0,0 +1,11 @@
+//if, else if , else
+
+let nivelDeFome = 1
+
+if(nivelDeFome === 1){
+   console.log("Pouca Fome")
+}else if(nivelDeFome === 2){
+   console.log("Muita fome")
+}else{
+   console.log("Não está com Fome")
+}
