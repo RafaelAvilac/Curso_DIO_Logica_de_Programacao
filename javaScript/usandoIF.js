3b7f0310625c18d41b7b799991de174cd50324fa
RafaelAvilac <rafael_avilac@yahoo.com.br>
2025-07-19 17:54:03 -0300

    Create usandoIF.js

---
+++ javaScript/usandoIF.js
@@ -0,0 +1,9 @@
+let ehLigado = false
+// Saída: "Não Executou"
+
+if(ehLigado) {                        // Testa se ehLigado é true
+    console.log("Executou comando")   // Bloco executado se true
+} 
+else {
+    console.log("Não Executou")       // Bloco executado se false
+}
