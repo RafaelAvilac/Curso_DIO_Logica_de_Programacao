e0290333db0b47707cb8b005e29999d2a8a1f210
RafaelAvilac <rafael_avilac@yahoo.com.br>
2025-07-20 19:22:44 -0300

    Update swith-case.js

---
+++ javaScript/swith-case.js
@@ -0,0 +1,21 @@
+//estrutura de decisão
+//switch/case/break/default
+
+let fruta = "goiaba";
+
+switch (fruta) {
+  case "laranja":
+    console.log("Suco de laranja");
+    break;
+
+  case "banana":
+    console.log("Vitamina de banana");
+    break;
+
+  case "maçã":
+    console.log("Suco de maçã");
+    break;
+
+  default:
+    console.log("Fruta não encontrada");
+}
