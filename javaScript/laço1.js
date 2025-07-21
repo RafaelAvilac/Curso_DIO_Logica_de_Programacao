2c3257cf3fa0702ee85b47239a25bea6993f5d85
RafaelAvilac <rafael_avilac@yahoo.com.br>
2025-07-21 20:20:17 -0300

    new

---
+++ javaScript/laço1.js
@@ -0,0 +1,8 @@
+//for
+let pontosDeVida = 0
+
+for(let i = 0; i <= 11; i++){
+    pontosDeVida += 1
+    console.log("Tomou poção mágica " + i)
+}
+console.log("Totais " + pontosDeVida)
---
+++ javaScript/while.js
@@ -0,0 +1,13 @@
+//while
+contador = 0
+while(contador < 3){
+    console.log("olá")
+    contador++
+}
+
+//do while
+let i = 0
+do{
+    console.log("Olá")
+    i++
+} while(i < 3)
