0a2be81134e7dbdb291398bda090e13f5b3b4be9
RafaelAvilac <rafael_avilac@yahoo.com.br>
2025-07-29 20:29:49 -0300

    Update função.js

--- javaScript/função.js
+++ javaScript/função.js
@@ -4,3 +4,10 @@ function dizerOla() {
 
 // Chamada da função
 dizerOla(); // Saída: Olá, Rafael!
+
+function somar(a, b) {
+  return a + b;
+}
+
+let resultado = somar(5, 3); // resultado agora vale 8
+console.log(resultado); // Saída: 8
