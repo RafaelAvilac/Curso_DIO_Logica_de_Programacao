e005f1b84962baf30b46a045666f3b9800302dd2
RafaelAvilac <rafael_avilac@yahoo.com.br>
2025-07-30 20:35:26 -0300

    Create condições.js

---
+++ javaScript/treino/condições.js
@@ -0,0 +1,34 @@
+//atualize a variável subscribe para que a comparação retorne true e o texto seja exibido no console
+
+let subscribe = true;
+
+if (subscribe) {
+  console.log("Thank you for subscribe!!!");
+}
+
+//Codifique uma declaração if que avalie se score é maior que 50. Dentro do bloco de código, exiba uma string com mais de 10 caracteres no console.
+
+let score = 100;
+if (score > 50) {
+  console.log("Parabéns sua pontuação é maior que 50");
+}
+
+//Na primeira condição, verifique se grade não é igual a "A".Na segunda condição, verifique se subject é igual a "Math".
+
+let subject = "Match";
+let grade = "A";
+
+if (grade !== "A") {
+  console.log("Give " + subject + " a chance");
+}
+
+if (subject === "Match") {
+  console.log("Today's a great day for solving equations!");
+}
+
+//Codifique uma condição para exibir o poder especial de um personagem se o character do jogo for um "Wizard".
+
+let character = "Wizard";
+if (character === "Wizard") {
+  console.log("Special power : potions");
+}
