f83505870320c8a819e099c9728e253ab4beba22
RafaelAvilac <rafael_avilac@yahoo.com.br>
2025-08-08 21:54:32 -0300

    Update função1.js
    Atualização do código

--- javaScript/função1.js
+++ javaScript/função1.js
@@ -1,10 +1,12 @@
 //Definição da função
 
-function torrar(pao) {
+function torrar(pao, nome = "cliente", valor) {
   console.log("Torrada feita com " + pao);
+  console.log("É um pedido de " + nome);
+  console.log("O valor é " + valor);
 }
 
 //Chamadas da função
 
-torrar("pão de forma.");
-torrar("pão integral.");
+torrar("pão de forma.", "Marcos", 11.63);
+torrar("pão integral.", "Victor", 15.3);
